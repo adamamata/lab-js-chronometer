f46feb97515129d3bd9bdae928e35a35ffd06cb9
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-07-28 12:05:39 +0200

    done

--- src/chronometer.js
+++ src/chronometer.js
@@ -34,6 +34,6 @@ class Chronometer {
   }
 
   split() {
-    // ... your code goes here
+    return `${this.computeTwoDigitNumber(this.getMinutes())}:${this.computeTwoDigitNumber(this.getSeconds())}`;
   }
 }
